2da3e0e0e06225bb2c903dcee91e6ba863fcfe81
marceloseiji <ikuta.marcelo@gmail.com>
2021-02-20 12:33:44 -0300

    Remove inused expense data

--- src/app.js
+++ src/app.js
@@ -12,17 +12,6 @@ import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
 
-store.dispatch(
-  addExpense({ description: "Water Bill", amount: 50, createdAt: 500 })
-);
-store.dispatch(
-  addExpense({ description: "Gas Bill", amount: 150, createdAt: 1000 })
-);
-store.dispatch(addExpense({ description: "Rent", amount: 109500 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
